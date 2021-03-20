d7d8884fa1ca04c30cf89784b51bec7faa4d63a6
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2021-03-21 01:54:56 +0900

    Update Practicum00-Hello_Assembly.asm

--- Practicum00-Hello_Assembly.asm
+++ Practicum00-Hello_Assembly.asm
@@ -1,1 +1,12 @@
-ADD r1, r2, r3
+		AREA	HelloW, CODE, READONLY
+SWI_WriteC		EQU	&0
+SWI_Exit			EQU	&11
+		ENRTY
+START		ADR	r1, TEXT
+LOOP		LDRB	r0, [r1], #1
+		CM	r0, #0
+		SWINE	SWI_WriteC
+		BNE	LOOP
+		SWI	SWI_Exit
+TEXT		=	"Hello World",&0a,&0d,0
+		END
